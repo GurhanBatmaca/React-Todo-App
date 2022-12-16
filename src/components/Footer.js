e3410513fe369b37a2bcb9ed32ec19dca19b303d
GurhanBatmaca <gurhanbtmc@hotmail.com>
2022-12-16 15:29:47 +0300

    seçililerin görünmesi tamalandı

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import { useState, useEffect } from 'react';
 
 const Footer = ( {formProp, setFormProp, showProp, setShowProp} ) => {
+
+  const [itemCount, setItemCount] = useState(formProp.length);
+
+  // let count = formProp.filter((item) => {
+  //   return item.active == true;
+  // })
+
+  useEffect(() => {
+    setItemCount(formProp.length);
+  }, [formProp])
+
   return (
     <footer>
       <div>
-        <span>{formProp.length} item left</span>
+        <span>{itemCount} item left</span>
         <button onClick={() => {setShowProp("all")}}>All</button>
         <button onClick={() => {setShowProp("acvite")}}>Active</button>
         <button onClick={() => {setShowProp("completed")}}>Completed</button>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,59 +3,48 @@ import { useState, useEffect } from 'react';
 const Main = ( {formProp, setFormProp, showProp} ) => {
 
   const [deleteItem, setDeleteItem] = useState("");
-  const [showTodo, setShowTodo] = useState(formProp)
+  const [showTodo, setShowTodo] = useState([]);
 
   const deleteBtn = (e) => {
     setDeleteItem(e);
-    // console.log("click");
   };
 
-  const todos = formProp.filter((item) => {
+  const onAfterDeletedItems = formProp.filter((item) => {
     return item.id !== deleteItem;
   });
 
   const onCheck = (item) => {
     formProp.map((i) => {
       if(i.id == item) {
-        // console.log(item);
         i.active = !i.active;
-        // console.log(i.active);
       }
     })
   };
 
   useEffect(() => {
-    setFormProp(todos);
+    setFormProp(onAfterDeletedItems);
   },[deleteItem]);
 
   let showList = formProp.filter( item => item);
 
-
   useEffect(() => {
-    setShowTodo(showList);
     if(showProp=="all") {
-      console.log("all")
       showList = formProp.filter((item) => {
         return item.active == true || item.active == false
       })
       setShowTodo(showList);
-      console.log(showList);
     } else if(showProp=="acvite") {
-      console.log("active");
       showList = formProp.filter((item) => {
         return item.active == true
       })
       setShowTodo(showList);
-      console.log(showList);
     } else if(showProp=="completed") {
-      console.log("completed");
       showList = formProp.filter((item) => {
         return item.active == false
       })
       setShowTodo(showList);
-      console.log(showList);
     } 
-  },[showProp]);
+  },[showProp,formProp]);
 
   return (
     <main>
@@ -69,7 +58,11 @@ const Main = ( {formProp, setFormProp, showProp} ) => {
                     type="checkbox"
                     id={item.id}
                   />
-                  <span className='text-span'>{item.text}</span>
+                  {
+                    item.active ? 
+                      <span className='text-span'>{item.text}</span> 
+                    : <span className='text-span'><del>{item.text}</del></span>
+                  }
                 </span>
                 <button
                   onClick={() => {deleteBtn(item.id)}}
